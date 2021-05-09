b0317a684ecfaf8cd61861f2dcb00f1a8404d8cd
EC2 Default User <ec2-user@ip-172-31-68-116.ec2.internal>
2021-05-09 12:01:35 +0000

    [Finish]4-3モデルとデータベースを用意しよう

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_115312) do
+ActiveRecord::Schema.define(version: 2021_05_09_115548) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
   end
 
 end
