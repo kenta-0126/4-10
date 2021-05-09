140d08d53576c1340abf437edb591f2e5ec7a1df
EC2 Default User <ec2-user@ip-172-31-68-116.ec2.internal>
2021-05-09 12:38:18 +0000

    [ADD]データの保存を行うcreateアクションを追加

--- app/controllers/tablists_controller.rb
+++ app/controllers/tablists_controller.rb
@@ -2,4 +2,17 @@ class TablistsController < ApplicationController
   def new
     @list = List.new
   end
+
+def create
+  list = List.new(list_params)
+  list.save
+  redirect_to
+end
+
+private
+
+def list_params
+  params.require(:list).permit(:title, :body)
+end
+
 end
